5012d1785de5cd58d6842145b415938daea25923
pipi9baby <pipi9baby@gmail.com>
2017-06-06 12:07:46 +0800

    改完merge跑步出來的部分

--- newick/merge.py
+++ newick/merge.py
@@ -2,9 +2,9 @@
 import re
 import os
 
-def checkEqual(array):
+def checkEqual(array,st,ed):
     for i in range(1,len(array)):
-        if array[i] != array[i-1]:
+        if array[i][st-1:ed] != array[i-1][st:ed+1]:
             return False
     return True
 def merge_br(filename, st, ed):
@@ -14,7 +14,7 @@ def merge_br(filename, st, ed):
     fr = open(filename,"r")
     data = fr.read()
     fr.close()
-    res = r"(\([^\()!']*\))([0-9||\.||:]*)"
+    res = r"(\([^\()]*\))([0-9||\.||:]*)"
     pattern = re.compile(res)
 
     count = 1
@@ -26,27 +26,29 @@ def merge_br(filename, st, ed):
             mh_str = match.group(1)
             pe_di = match.group(2)
             br = mh_str.split(",")
-            seq_name.append(br[0][:br[0].find(":")])
-            br[0] = br[0][st:ed+1]
-            for i in range(1,len(br)):
-                seq_name.append(br[i][:br[i].find(":")])
-                br[i] = br[i][st-1:ed]
-
-            if checkEqual(br):
-                count = count + 1
+            #seq_name.append(br[0][:br[0].find(":")])
+            #br[0] = br[0][st:ed+1]
+            if checkEqual(br,st,ed) == True:
+                count = count+1
                 tmp = ""
                 for i in range(0,len(br)):
-                    tmp = tmp + seq_name[i] + "|"
+                    tmp = tmp + br[i][:br[i].rfind(":")-1] + "|"
                 tmp = tmp[:-1] + pe_di[pe_di.find(":"):]
-                data = data.replace(match.group(0),tmp,1)
+                data = data.replace(match.group(0),tmp[1:-1],1)
                 #print(tmp1,tmp2)
                 #print (match.groups(i+1))
-    print(count)
+        print(count)
+
     fw = open("output.nwk","w")
     fw.write(data)
     fw.close()
     filename = "output.nwk"
-    """
+"""
+st = int(input("匹配起始位置："))
+ed = int(input("匹配結束位置："))
+filename = "testData\V2R_rootedML_0222.nwk"#input("請輸入檔案名稱(含附檔名)：")
+merge_br(filename, st, ed)
+
     res = "\)[0-9||.]*:"
     pattern = re.compile(res)
     for i, line in enumerate(open(filename)):
@@ -58,5 +60,4 @@ def merge_br(filename, st, ed):
     fw.write(data)
     fw.close()
     filename = "output.nwk"
-    """
-    print("success!!")
+"""
